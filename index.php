024557768d39f4de38e5715a01365f0d9b4e7f54
Matthieu Afane <matthieu.afane@etudiant.univ-reims.fr>
2025-10-04 19:19:10 +0200

    feat: ajout des polices Clash Display, images et fichiers de base (style.css, script.js, config.php)

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+echo "test";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <h1>Bonjour</h1>
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam obcaecati blanditiis eos quis, veniam nostrum deleniti eligendi vitae maxime harum minus optio dolor a earum. Ratione iusto voluptas illo saepe.</p>
+</body>
+</html>
